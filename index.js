1b775620f36292545d73227ef669a96cc1ebb4f4
Min Thu Zaw <minthuzaw775@gmail.com>
2023-02-28 09:02:23 +0630

    added: message data emit and listen events

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let express = require('express')
+let socket = require('socket.io')
 
 // app setup
 let app = express()
@@ -11,4 +12,14 @@ let server = app.listen(4000, () => {
 // route setup
 app.get('/', (res, req) => {
     req.sendFile(__dirname + "/public/index.html");
+})
+
+// socket setup
+let io = socket(server)
+io.on('connection', (socket) => {
+    // console.log("Socket connection connected " + socket.id)
+    socket.on("chat", (data) => {
+        // console.log(data)
+        io.sockets.emit("chat", data);
+    });
 })
